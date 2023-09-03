2c342aba4dea0121afc14bd791690c0c1fccd3f9
Judy <judysoukkhaphon@gmail.com>
2023-09-03 06:19:01 -0400

    form to edit server data is now working correctly. Removed event.preventDefault from the submit function in Form, added a function in Templates to PUT data, the onChange function now also takes the placeholder value as well because it holds that fields original value, so if the user starts to make a change to that field but changes their mind, then the old value will still be there. Created a new component called Creator which will be used to create server data.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,92 +1,36 @@
-import {React, useState} from "react";
+import {React} from "react";
 
 function Form(props) {
-    const [objData, setObjData] = useState()
-    const [obj, setObj] = useState(props.data);
-    const [title, setTitle] = useState(props.title);
+    const objArray = Object.entries(props.data);
+    const form = props.data;
 
-    
-    //console.log(props);
-    //console.log(props.objectData);
-    /*
-        props = {[{id:test}, {id:test2}]}
-    */
-
-        const data = props.data;
-        console.log(data);
-        const objArray = Object.entries(data);
-
-    /*
-        propData = {
-            0 : {custom[1]},
-            1 : {custom[2]}
-        }
-        propData = {index, objects}
-    */
-
-       // console.log(Object.entries(props.objectData["data"]));
-    //works, should this be a state var???
     function handleNewTitle(event) {
         event.preventDefault();
         let objId = event.target.value;
         console.log(objId);
     }
 
-    // works, simple regular HTML like form handling
-    function handledBValue(event, key){
+    function handledBValue(event, key, oldValue){
         event.preventDefault();
-        let newValue = event.target.value.key;
-        setObjData(event.target.value);
-        console.log(newValue);
-        
         if(event.target.value !== ""){
-            console.log(objData);
-
+            form[key] = event.target.value;
         } else{
-             console.log("dropdown option was null")
+             form[key] = oldValue;
         }
     }
-
-    
 // -------------<Input> tags creator-----------------
-
     const formViewer = objArray.map((item)=>{
         const index = objArray.indexOf(item);
-        console.log(Object.entries(data)[`${title}`]); // [[key:value], [key:value]]
-        console.log((Object.entries(data)))
-        // key = Object.entries(data)[0][0]
-        // value = Object.entries(data)[0][1]
-        //item = Object.entries(data)[0] = ['key', 'value']
-        console.log(Object.entries(data)[{index}]); // ['key', 'value']
-        //const key = ${props.title}${index};
         return(
             <div key={`${props.title}${index}`}>
-            {/*<label key={`${props.title}${index}`}>{item[0]}</label>*/}
-            <input type="text" key={`${props.title}${index}`} placeholder={item[1]} onChange={(event)=>handledBValue(event, item[0])}/>
-            <label>{`value ${index}`}</label>
+           <label>{item[0]}</label>
+            <input type="text" key={`${props.title}${index}`} placeholder={item[1]} onChange={(event)=>handledBValue(event, item[0], item[1])}/>
             </div>
         )
     })
 
-
-    function print(event) {
-        event.preventDefault();
-        console.log();
-        console.log(event.target.value);
-        console.log(formViewer);
-        /*
-        [{lab}]
-
-        */
-        /*
-        fetch(`http://localhost:3000/custom/${title}`,{
-            method: "PUT",
-            body: JSON.stringify({
-                data:testObj}),
-            headers: {"Content-type": "application/JSON; charset=UTF-8"}
-        }) .then((response) => response.json())
-        .then((newData) => {console.log(newData); setData((oldData)=>[...Object.entries(newData.data)])});
-        */
+    function print() {
+        props.func(form);
     }
 
     return(
--- src/components/Templates.js
+++ src/components/Templates.js
@@ -3,26 +3,18 @@ import Form from "./Form"
 
 function Templates (){
     const [option, setOption] = useState("");
-    const [select, setSelect] = useState([]);
+    const [selections, setSelections] = useState([]);
     const [serverData, setData] = useState([]);
-
     const [title, setTitle] = useState("");
 
-    console.log("Component restart");
-
     useEffect(() => {
-        console.log("useEffect");
         fetch(`http://localhost:3000/custom/${option}`)
         .then((response)=>response.json())
         .then((arrayOfServerData) => {
-          console.log("fetched");
           JSON.stringify(arrayOfServerData);
-          console.log(arrayOfServerData);
           if(Array.isArray(arrayOfServerData)) {
-            setSelect(arrayOfServerData);
-            //setData({id:"", data:{}});
+            setSelections(arrayOfServerData);
           } else {
-            console.log("not Array")
             setData(arrayOfServerData.data);
             setTitle(arrayOfServerData.id);
           }
@@ -35,63 +27,33 @@ function Templates (){
         setData([]);
     }
 
-    /*
-    function getFetch() {
-        console.log("getFetch");
-        if(option){
-            console.log("if (option) = true");
-            URI = {option};
-            console.log(URI);
-        } else {
-            URI = "";
-            console.log("else, (option) = false");
-        }
-        console.log(URI);
-        fetch(`http://localhost:3000/custom/${URI}`)
-        .then((response)=>response.json())
-        .then((arrayOfServerData) => {
-            console.log("fetched");
-            JSON.stringify(arrayOfServerData);
-            console.log(arrayOfServerData);
-            setData(arrayOfServerData);})
-    }
-    */
-
-    //console.log(serverData); 
-    /* RETURNS:
-        serverData = [
-            [{id, data}],
-            [{id, data}]
-        ]
-    */
-
-     // ----- HANDLERS ----- only finds info, makes no changes
     function dropdownHandler(event) {
         setOption(event.target.value);
-        //setURI(`${event.target.value}`);
-        //testfn(serverData);
     }
 
-    // ----- DOM creators ----
-    // const dropdown only creates an array of the data, which is used as a reference to build the dropdown menu
-    // data here directly takes the jsonified server data and its ONLY used to build the dropdown
-    // no data is changed
-    const dropdown = select.map((object) =>{
+    function postData(objData) {
+        fetch(`http://localhost:3000/custom/${title}`, {
+            method: "PUT",
+            body: JSON.stringify({
+                data:objData}),
+            headers: {"Content-type": "application/JSON; charset=UTF-8"}
+        }) .then((response) => response.json())
+        .then((newData)=> console.log(newData));
+    }
+
+    const dropdown = selections.map((object) =>{
         return(
             <option key={object.id} value={object.id}>{object.id}</option>
      )})
 
-    function myChild(){
-        console.log("no")
-    }
 
     return(
         <div>
-            <select onChange={dropdownHandler}>
+            <select id="titles" onChange={dropdownHandler}>
                 <option value=""></option>
                 {dropdown}
             </select>
-            <Form title={title} data={serverData}/>
+            <Form title={title} data={serverData} func={postData}/>
             <p> Make sure the input field text always match actual value</p>
         </div>
     )
